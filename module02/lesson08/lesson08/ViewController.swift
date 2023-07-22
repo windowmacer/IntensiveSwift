595e8718a783e8d13ca7c92375888fb6c8b2896c
windowmacer <kirill.arefev.98@mail.ru>
2023-07-22 15:27:04 +0300

    22/07/2023 - lesson08: edit collectionSetup()

--- module02/lesson08/lesson08/ViewController.swift
+++ module02/lesson08/lesson08/ViewController.swift
@@ -12,18 +12,53 @@ class ViewController: UIViewController {
 	var collection: UICollectionView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		view.backgroundColor = .darkGray
 		collectionSetup()
 	}
 
 	private func collectionSetup() {
 		let layout = UICollectionViewFlowLayout()
 		layout.scrollDirection = .horizontal
-		layout.minimumLineSpacing = 0
-		layout.minimumInteritemSpacing = 0
-		layout.itemSize = CGSize(width: 100, height: 100)
+		layout.minimumLineSpacing = 12
+		layout.minimumInteritemSpacing = 12
+		layout.itemSize = CGSize(width: 160, height: 90)
 		
 		collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+		collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+		collection.dataSource = self
+		collection.delegate = self
+		collection.backgroundColor = .gray
+		collection.layer.cornerRadius = 10
+		
+		collection.translatesAutoresizingMaskIntoConstraints = false
 		self.view.addSubview(collection)
+		
+		NSLayoutConstraint.activate([
+			collection.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+			collection.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+			collection.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+			collection.heightAnchor.constraint(equalToConstant: 200),
+		])
 	}
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+	
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		10
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+		
+		cell.contentView.backgroundColor = .white
+		
+		return cell
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		return CGSize(width: 320, height: 180)
+	}
+	
+	
+}
