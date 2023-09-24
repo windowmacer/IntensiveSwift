d003b5c3a070d9d5c7cc0d7e7d39a600e3d42d97
windowmacer <kirill@192.168.1.10>
2023-09-24 19:17:56 +0300

    24/09/2023 - finish: module04/lesson02

--- module04/lesson02/lesson02/ContentView.swift
+++ module04/lesson02/lesson02/ContentView.swift
@@ -8,45 +8,94 @@
 import SwiftUI
 
 struct ContentView: View {
-	@State var login = String()
-	@State var password = String()
+	@State var login = ""
+	@State var password = ""
+	@State var isEditinngEmail = false
+	@State var isEditinngPassword = false
 	
-    var body: some View {
-        VStack {
-			HStack {
-				Image(systemName: "person.crop.circle")
+	var body: some View {
+		VStack(spacing: 24) {
+			Spacer()
+			HStack(spacing: 15) {
+				Image("AvatarImage")
 					.resizable()
-					.frame(width: 100, height: 100)
-				VStack {
-					Text("email@mail")
-					Text("password")
+					.frame(width: 120, height: 120)
+					.cornerRadius(40)
+				
+				VStack(alignment: .leading, spacing: 6) {
+					TextUser(name: login)
+					TextUser(name: password)
 				}
+				Spacer()
 			}
-			.background(.red)
-			.cornerRadius(10)
+			.frame(maxWidth: .infinity)
+			.padding(.horizontal, 27)
 			
-			VStack {
-				TextField("Login", text: $login)
-					.padding()
-					.background(.gray)
+			VStack(spacing: 22) {
+				TextField("email@mail.com", text: $login)
+					.padding(.vertical, 16)
+					.padding(.leading, 27)
+					.background(Color("TextFieldColor"))
+					.font(.system(size: 16))
+					.foregroundColor(isEditinngEmail ? .black : Color("TextColor"))
 					.cornerRadius(10)
-				TextField("Password", text: $password)
-					.padding()
-					.background(.gray)
+					.onTapGesture {
+						isEditinngEmail = true
+					}
+			
+				SecureField("password", text: $password)
+					.padding(.vertical, 16)
+					.padding(.leading, 27)
+					.background(Color("TextFieldColor"))
+					.font(.system(size: 16))
+					.foregroundColor(isEditinngPassword ? .black : Color("TextColor"))
 					.cornerRadius(10)
+					.onTapGesture {
+						isEditinngPassword = true
+					}
+			
+				Button {
+					// action
+				} label: {
+					Text("Log In")
+						.frame(maxWidth: .infinity)
+						.padding(.vertical, 16)
+						.background(Color("ButtonColor"))
+						.font(.system(size: 16))
+						.foregroundColor(.white)
+						.cornerRadius(10)
+				}
+				
 			}
+			.frame(maxWidth: .infinity)
+			.padding(.horizontal, 27)
+			.padding(.vertical, 39)
 			.background(.white)
+			.cornerRadius(20)
 			
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-			Text("Lol!")
-				.padding(10)
-        }
-        .padding()
-		.background(.gray)
-    }
+			Button("Registration") {
+				// action
+			}
+			.foregroundColor(.white)
+			.font(.system(size: 16))
+			
+			Spacer()
+			
+		}
+		.padding(.horizontal, 17)
+		.frame(maxWidth: .infinity, maxHeight: .infinity)
+		.background(Color("MainColor"))
+	}
+}
+
+struct TextUser: View {
+	var name: String
+	var body: some View {
+		Text(name)
+			.foregroundColor(.white)
+			.font(.system(size: 16))
+			.bold()
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
